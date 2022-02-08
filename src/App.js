3883b265ec62b29fe87932fade5b19c7da701291
ShaheerDev <64355920+ShaheerDev@users.noreply.github.com>
2022-02-09 00:21:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import bulbON from './imgs/bulbOn.jpg';
+import bulbOFF from './imgs/bulbOff.jpg';
 
 function App() {
+  let [bgcolor, setBgcolor] = useState(false);
+  let [bulb, setBulb] = useState(false)
+
+  const changeTheme = () => {
+    setBgcolor(!bgcolor)
+  }
+
+  const changeBulb = () => {
+    setBulb(!bulb)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: bgcolor ? 'black' : 'white', minHeight: '100vh' }}>
+      <h1 style={{color: bgcolor?'white':'black'}}>Current theme: {bgcolor?'Dark theme':'Light theme'}</h1>
+      <button style={{backgroundColor: 'white', borderRadius: '6px', padding: '8px',}} onClick={()=>changeTheme()}>Toggle Theme</button>
+      <h1 style={{color: bgcolor?'white':'black'}}>Bulb is {bulb?'Off':'On'}</h1>
+      <button style={{backgroundColor: 'white', borderRadius: '6px', padding: '8px',}} onClick={()=>changeBulb()}>Turn {bulb?'On':'Off'} Bulb</button>
+      <br></br>
+      <img src={bulb?bulbOFF:bulbON} alt='bulbImg'></img>
     </div>
   );
 }
